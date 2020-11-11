6b155207e3780f70be7b5bfb40633073023b2caf
Daviduf11 <ufberg.david@gmail.com>
2020-11-10 23:57:51 -0500

    cleaned up spacing

--- Battle-for-Wyncode.js
+++ Battle-for-Wyncode.js
@@ -21,7 +21,6 @@ if (userCharacter === "Ernie") {
 
 console.log("\nThe Battle For Wyncode is a turn-based action game. To win, accumulate five victories before any of your rivals.\n\nEach turn you can choose from three actions:\n\n • Shop - Purchase items. Prices fluctuate so look out for good deals!\n\n • Battle - Face off against a rival. Before battling, you can roll a die to take on a Battle Effect that will last for one battle. The winner of each battle will gain a Victory.\n\n • Fortify - Gain Strength.\n\nEach turn your rivals can also Battle each other or Fortify. Make sure you get to five Victories before any of them!");
 
-
 charactersList.splice(charactersList.indexOf(userCharacter), 1);
 
 prompt.question("\nPress 'Enter' to start.\n\n");
